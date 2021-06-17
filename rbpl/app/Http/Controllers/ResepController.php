000f04e13b00824b10b9564e093055706cd6564f
DwikiCahyo <70952085+DwikiCahyo@users.noreply.github.com>
2021-06-17 12:02:20 +0700

    exp Views
    contoh views list resep

--- rbpl/app/Http/Controllers/ResepController.php
+++ rbpl/app/Http/Controllers/ResepController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Resep;
 use Illuminate\Http\Request;
 
 class ResepController extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+    public function viewlistresep(){
+       $resep = Resep::all();
+       return view ('pages.viewlistresep',['reseps'=>$resep]);
+
+    }
+
 }
--- rbpl/routes/web.php
+++ rbpl/routes/web.php
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
+Route::get('/listresep', 'App\Http\Controllers\ResepController@viewlistresep');
 
 Auth::routes();
 
