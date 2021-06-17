a7bbd44f11e606f381197d879392384593b1f1cf
DwikiCahyo <70952085+DwikiCahyo@users.noreply.github.com>
2021-06-17 12:09:15 +0700

    update controller resep

--- rbpl/app/Http/Controllers/ResepController.php
+++ rbpl/app/Http/Controllers/ResepController.php
@@ -8,10 +8,7 @@ use Illuminate\Http\Request;
 class ResepController extends Controller
 {
     //
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+  
 
 
     public function viewlistresep(){
