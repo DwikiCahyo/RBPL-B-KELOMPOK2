7694930728fa70fc63e95b85b9eeac885757aa63
DwikiCahyo <70952085+DwikiCahyo@users.noreply.github.com>
2021-06-27 10:59:28 +0700

    tempat

--- app/Http/Controllers/TempatController.php
+++ app/Http/Controllers/TempatController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tempat;
 use Illuminate\Http\Request;
 
 class TempatController extends Controller
 {
-    //
+    public function viewlisttempatadmin(){
+        $tempat = Tempat::all();
+        return view ('tempat.listtempatadmin',['tempats'=>$tempat]);
+ 
+     }
 }
