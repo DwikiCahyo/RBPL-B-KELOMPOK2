8b7b0ea55509a3b641e44f6b42c03ede90985440
alfadoraflyh <72862438+alfadoraflyh@users.noreply.github.com>
2021-06-28 20:32:14 +0700

    update list tempat admin

--- resources/views/tempat/listtempatadmin.blade.php
+++ resources/views/tempat/listtempatadmin.blade.php
@@ -1,29 +1,42 @@
 @extends('layouts.receppe')
-@section('title','List Resep')
-
+@section('title','List Tempat')
 @section('content')
-<div class="container">
-    <div class="row">
-      <div class="col-sm-3"></div>
-      <div class="col-sm-6"></div>
-        <div class="container">
-              <div class="card">
-                <div class="card-body">
-                    <table>
-                        <tr>
-                            <td border-spacing: 10px;><img src="https://th.bing.com/th/id/Re118e163aa96e26e71b14df1d9699374?rik=ZRdnlZir5rDWZg&riu=http%3a%2f%2fmixedupalready.com%2fwp-content%2fuploads%2f2016%2f01%2fIndonesia-Surabaya-Pop-Hotels-Pop-Hotel-Stasiun-Kota-Near-Pasar-Atom.jpg%3fx84758&ehk=A9LJYSVVzek5vIeBEqLXzV0ygv91tMfW6r2MI8OLIQw%3d&risl=&pid=ImgRaw" width="200" height="100" alt=""></td>
-                            <td>
-                                <h3>head</h3>
-                                <p>Pasar Atom merupakan pasar legendaris di Surabaya yang menjadi pusat sentral kunjungan wisata maupun kunjungan pedagang dari daerah Indonesia Timur sehingga target pasar pengunjung Pasar Atom tidak hanya mencakup wilayah Surabaya saja tapi mencakup Indonesia Timur.
-                                Pasar Atom sebagai pusat perbelanjaan dengan icon-iconnya yang khas menarik. Belanja mulai dari tekstil, emas, makanan, jajanan pasar, camilan, P&D, pakaian, sepatu, penjahit, bed cover & sprei, aksesories, mainan anak-anak dan fancy, souvenir, stationery, furniture hingga
-                                institusi perbankan. Salah satu hal yang menarik ketika berbelanja di Pasar Atom adalah seni tawar menawar antara penjual dan pembeli pada saat transaksi karena mayoritas stand yang ada langsung dijaga oleh pemiliknya sendiri. Ini merupakan nilai tambah yang dimiliki Pasar Atom
-                                dibanding dengan pusat perbelanjaan lain yang ada di Surabaya.</p>
-                            </td>
-                        </tr>
-                    </table>
+<div class="row">
+    <div class="col-md-2"></div>
+    <div class="col-md-8">
+        <div class="Container mt-5">
+            <div class="card">
+                <div class="row">
+                    <div class="col-md-4">
+                    <img src="https://th.bing.com/th/id/OIP.fYga__wywybMq81k0cGrJwHaE8?pid=ImgDet&rs=1" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-8">
+                    <h2>Pasar Atom</h2>
+                    <p>Pasar Atom adalah sebuah pusat perbelanjaan di Surabaya. Pasar ini dikenal salah satunya karena penggunaan bahasa
+                    gado-gado oleh para pedagang (dan juga pembeli) yang mayoritas beretnis Tionghoa. Bahasa Pasar Atom menjadi begitu khas, sehingga penggunaan bahasa
+                    serupa di luar Pasar Atom pun juga disebut demikian.</p>
+                    <a href="#" class="btn btn-primary">Lihat</a>
+                    </div>
                 </div>
-              </div>
+            </div>
         </div>
-      <div class="col-sm-3"></div>
+        <div class="Container mt-5">
+            <div class="card">
+                <div class="row">
+                    <div class="col-md-4">
+                    <img src="https://th.bing.com/th/id/OIP.fYga__wywybMq81k0cGrJwHaE8?pid=ImgDet&rs=1" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-8">
+                    <h2>Pasar Atom</h2>
+                    <p>Pasar Atom adalah sebuah pusat perbelanjaan di Surabaya. Pasar ini dikenal salah satunya karena penggunaan bahasa
+                    gado-gado oleh para pedagang (dan juga pembeli) yang mayoritas beretnis Tionghoa. Bahasa Pasar Atom menjadi begitu khas, sehingga penggunaan bahasa
+                    serupa di luar Pasar Atom pun juga disebut demikian.</p>
+                    <a href="#" class="btn btn-primary">Lihat</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-2"></div>
 </div>
 @endsection
