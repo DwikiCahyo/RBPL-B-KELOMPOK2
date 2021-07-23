7c6326c1d8ee9443ddfdbb4045c9c211b11f9787
Galuh173 <72306824+Galuh173@users.noreply.github.com>
2021-07-23 11:00:31 -0700

    Artikel Test
    Ramadhani Galuh Candra Purtiwi (05211940000037)
    Ika Rahma Andhani (05211940000044)

--- tests/Feature/ArtikelTest.php
+++ tests/Feature/ArtikelTest.php
@@ -1,22 +1,58 @@
 <?php
-
+//Ramadhani Galuh Candra Purtiwi (05211940000037)
+//Ika Rahma Andhani (05211940000044)
 namespace Tests\Feature;
 
+use App\Models\Artikel;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ArtikelTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    /** @test */
+    public function menambah_artikel()
     {
         $response = $this->get('/');
+        Artikel::factory()->create([
+            'JudulArtikel' => 'Manfaat Tomat untuk kesehatan',
+            'DeskripsiArtikel' => 'Tomat merupakan salah satu sayuran yang kaya akan nutrisi'
 
+        ]);
         $response->assertStatus(200);
+    }
+    /** @test */
+    public function menampilkan_list_artikel()
+    {
+        $this->get(route('listArtikel'));
+        $this->assertTrue(true);
+    }
+    /** @test */
+    public function menampilkan_list_artikel_admin()
+    {
+        $this->get(route('listArtikelAdmin'));
+        $this->assertTrue(true);
+    }
+    /** @test */
+    public function menghapus_artikel()
+     {
+        $response = $this->get('/');
+       $tempats=Artikel::factory()->create([
+        'JudulArtikel' => 'Manfaat Mangga untuk kesehatan',
+        'DeskripsiArtikel' => 'Mangga merupakan salah satu buah yang kaya akan nutrisi'
+
+        ]);
+
+        $this->assertDatabaseHas('artikels',[
+            'JudulArtikel' => 'Manfaat Mangga untuk kesehatan',
+            'DeskripsiArtikel' => 'Mangga merupakan salah satu buah yang kaya akan nutrisi'
+
+        ]);
+        $this->get(route('listArtikelAdmin'));
+
+        $delete_tempat = Artikel::destroy($tempats->id);
+
+        $this->assertTrue(true);
+     
     }
 }
